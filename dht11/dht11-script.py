f4ded7e914018ec5042a06c17bc6de9acb0e591c
Declan Currey <declan.currey@gmail.com>
2020-06-21 22:21:50 +0000

    adds exception handling to dht11 script for keyboard input

--- dht11/dht11-script.py
+++ dht11/dht11-script.py
@@ -14,23 +14,28 @@ def log_error(error_code):
     with open("./logs/dht11-error-logs.txt", "a") as log_file:
         log_file.write(" Error Logged! With error code {} at {} \n ".format(error_code, time.ctime()))
 
+try: 
+    while True:
 
-while True:
+        # read from the dht11 sensor
+        result = instance.read()
 
-    # read from the dht11 sensor
-    result = instance.read()
-
-    # if the reading from the dht11 is valid, print temperature
-    if result.is_valid():
-        print("****************************************************")
-        print("Time reading recorded: " + time.ctime())
-        print("Temperature: %-3.1f C" % result.temperature)
-        print("Humidity: %-3.1f %%" % result.humidity)
-        print("**************************************************** \n")
-       
-        # sleep the script for 2.5 seconds
-        time.sleep(2.5)
-    else:
-        log_error(result.error_code)
+        # if the reading from the dht11 is valid, print temperature
+        if result.is_valid():
+            print("****************************************************")
+            print("Time reading recorded: " + time.ctime())
+            print("Temperature: %-3.1f C" % result.temperature)
+            print("Humidity: %-3.1f %%" % result.humidity)
+            print("**************************************************** \n")
+           
+            # sleep the script for 2.5 seconds
+            time.sleep(2.5)
+        else:
+            log_error(result.error_code)
+except KeyboardInterrupt:
+    print("Script interrupted via keyboard input... Cleaing up GPIO.")
+    GPIO.cleanup()
+    print("Script exited successfully.")
+    
 
         
